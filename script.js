36e319ad89c8729d1994977fbf90eb7a5654d4b0
Gaurav Madan <gauravmadan2004@gmail.com>
2023-04-10 23:02:16 +0530

    commit

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.navlinks');
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('cross');
-  navLinks.classList.toggle('nav-active');
-});
