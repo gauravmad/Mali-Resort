5a71bbfd9b8e79f0ccd6a397e2a26ea29dae2c88
Gaurav Madan <gauravmadan2004@gmail.com>
2023-05-07 22:51:53 +0530

    commit

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
