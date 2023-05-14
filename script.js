d36f6390a4df56b77c02de0c06c21b5f505eb9a1
Gaurav Madan <gauravmadan2004@gmail.com>
2023-05-15 01:22:32 +0530

    commit

--- script.js
+++ script.js
@@ -6,33 +6,3 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('nav-active');
 });
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  autoplay: {
-    delay: 1500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    // when window width is <= 991px
-    991: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    // when window width is <= 480px
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-  },
-});
-
