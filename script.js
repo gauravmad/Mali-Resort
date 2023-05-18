e76a86638de070abdfc8d4f4a579f0854e087feb
Gaurav Madan <gauravmadan2004@gmail.com>
2023-05-18 18:08:23 +0530

    commit

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('nav-active');
 });
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
